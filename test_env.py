3e03db29e69a376819d407632815280971242a4c
KNIGHT-29 <singhromit9211@gmail.com>
2025-12-21 23:58:05 +0530

    setup initial

---
+++ test_env.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+print("Environment setup successful")
+
